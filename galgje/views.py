142249c7cb72d8a44b05f1e0bee1a1cab1f7e696
Aad Schippers <aadschippers@gmail.com>
2023-02-01 14:50:40 +0100

    progress

--- galgje/views.py
+++ galgje/views.py
@@ -1,40 +1,71 @@
 from django.shortcuts import render
+import ast
 
 # Create your views here.
-status = 0
-word = ""
-guessword = ""
-guessedletters = []
 
 
 def index(request):
+    if request.method == 'GET':
+        try:
+            if not guessedletters:
+                gamevars = InitSpel()
+        except UnboundLocalError:
+            gamevars = InitSpel()
+
+        status = gamevars['status']
+        guessword = gamevars['guessword']
+        word = gamevars['word']
+        guessedletters = gamevars['guessedletters']
+        score = gamevars['score']
+        highscore = gamevars['highscore']
+
     if request.method == 'POST':
         letter = request.POST['letter']
+        status = request.POST['status']
+        guessword = request.POST['guessword']
+        word = request.POST['word']
+        guessedletters = ast.literal_eval(request.POST["guessedletters"])
+        score = request.POST['score']
+        highscore = request.POST['highscore']
 
         if letter == 'Nieuw':
-            InitSpel()
+            guessedletters = InitSpel()
         else:
-            RaadWoord(letter)
+            guessedletters = RaadWoord(letter, guessedletters)
 
     return render(
         request,
         "galgje/index.html", {
         'status': status,
         'guessword': guessword,
-        'guessedletters': guessedletters
+        'word': word,
+        'guessedletters': guessedletters,
+        'score': score,
+        'highscore': highscore,
         }
     )
 
 
 def InitSpel():
-    global guessedletters
-    guessedletters = []
+    word = "badjas"
+    guessword = "......"
+    score = 0
+    highscore = 0
+
+
+    gamevars = {
+        'guessedletters': [],
+        'status': 0,
+        'word': word,
+        'guessword': guessword,
+        'score': score,
+        'highscore': highscore,
+    }
     
-    return
+    return gamevars
 
 
-def RaadWoord(letter):
-    global guessedletters
+def RaadWoord(letter, guessedletters):
     guessedletters.append(letter)
     
-    return
+    return guessedletters
