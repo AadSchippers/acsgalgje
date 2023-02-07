0767888d9c6a79fe98951c817bead7f2ce1282e4
Aad Schippers <aadschippers@gmail.com>
2023-02-07 13:09:34 +0100

    config_sample.py added

---
+++ acsgalgje/config_sample.py
@@ -0,0 +1,15 @@
+# Copy this file to config.py
+
+# SECURITY WARNING: keep the secret key used in production secret!
+# A secret key is neccessary to run this site.
+# You can generate your SECRET_KEY here: https://djecrety.ir/
+SECRET_KEY = ''
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+# Add your site url to the list:
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    ]
